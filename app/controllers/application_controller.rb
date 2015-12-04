566fdfd3ccb7200af9ff300e71a75ad2a060c3e7
Bialy <bialy358@gmail.com>
2015-12-04 18:33:23 +0100

    Add before_action to App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :logged_in_user
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   decent_configuration { strategy DecentExposure::StrongParametersStrategy }
 
   protected
@@ -14,6 +15,13 @@ class ApplicationController < ActionController::Base
         password_confirmation
       )
       u.permit(sanitized_params)
+    end
+  end
+
+  def logged_in_user
+    unless user_signed_in?
+      flash[:danger] = "You need to sign in or sign up before continuing."
+      redirect_to new_user_session_url
     end
   end
 end

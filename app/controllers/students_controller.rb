09a829d343c74f1d657b3abf1fa8cd26f72f39e8
Bialy <bialy358@gmail.com>
2015-12-04 14:43:00 +0100

    Rapair student Model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,24 +3,12 @@ class StudentsController < ApplicationController
   expose(:student, attributes: :student_params)
   expose(:student_subject_items) { student.subject_items }
 
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
   def create
     if student.save
       redirect_to student_path(student), notice: I18n.t('shared.created', resource: 'Student')
     else
       render :new
     end
-  end
-
-  def edit
   end
 
   def update
@@ -32,7 +20,7 @@ class StudentsController < ApplicationController
   end
 
   def destroy
-    @student.destroy
+    student.destroy
     redirect_to students_path, notice: I18n.t('shared.deleted', resource: 'Student')
   end
 
